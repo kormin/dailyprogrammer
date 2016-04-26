59a38c7bfe5bec31f3746fff05ab4219b4136dbf
kormin <abaotom14@gmail.com>
2016-04-26 19:29:37 +0800

    update userinput cont

--- 1/userInput.php
+++ 1/userInput.php
@@ -23,5 +23,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			Enter name: <input type="text" name="name"> <br> <br>
+			Enter age: <input type="number" name="age" min="0" max="300"> <br> <br>
+			Enter reddit username: <input type="text" name="usrname"> <br> <br>
+			<input type="submit" value="Submit">
+		</form>
+		<br>
+		<h2>
+			<?php
+				if(!empty($_POST)) {
+					$name = $_POST['name'];
+					$age = $_POST['age'];
+					$usrname = $_POST['usrname'];
+					echo "your name is ".$name.", you are ".$age." years old, and your username is ".$usrname;
+				}
+			?>
+		</h2>
 	</body>
 </html>
