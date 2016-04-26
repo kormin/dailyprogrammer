ae2c5cdaed17b8e162e10d79f4c03276db4f88bd
kormin <abaotom14@gmail.com>
2016-04-26 19:28:04 +0800

    add userinput

---
+++ 1/userInput.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>[easy] challenge #1</title>
+		<meta charset="utf-8">
+		<meta name="description" content="programming challenge taken from /r/dailyprogrammer subreddit">
+		<meta name="keywords" content="php,html5,forms,inputs">
+		<link rel="author" href="https://github.com/kormin">
+		<!-- <link rel=canonical href=""> -->
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+	</body>
+</html>
