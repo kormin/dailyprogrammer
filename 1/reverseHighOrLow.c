83e3d8119782988c1e7df4b644298b5134cde4ef
kormin <abaotom14@gmail.com>
2016-04-27 19:44:28 +0800

    add reversehighorlow

---
+++ 1/reverseHighOrLow.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int num, fl=1, tries=0, ceil=101, flr=1;
+    char ch;
+    printf("Your number is: %d.\nAnswered in: %d tries.",num,tries);
+    return 0;
+}
