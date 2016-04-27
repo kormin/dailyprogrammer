996ffed9be111d941a0d8fa807c125624383ce21
kormin <abaotom14@gmail.com>
2016-04-27 19:50:03 +0800

    add file

---
+++ 1/eventCal.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>[intermediate] challenge #1</title>
+
+		<meta name="description" content="programming challenge taken from /r/dailyprogrammer subreddit">
+		<meta name="keywords" content="php,html5,forms,inputs">
+		<link rel="author" href="https://github.com/kormin">
+	</head>
+	<body>
+	</body>
+</html>
