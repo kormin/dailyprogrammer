5bc74a6de12d1f6cf687c3e8d77a059ba7264eb3
kormin <abaotom14@gmail.com>
2016-04-27 19:44:42 +0800

    update reversehighorlow

--- 1/reverseHighOrLow.c
+++ 1/reverseHighOrLow.c
@@ -4,6 +4,20 @@
 int main(){
     int num, fl=1, tries=0, ceil=101, flr=1;
     char ch;
+    for(tries=0;fl;++tries){
+        //system("cls");
+        num = (ceil+flr)/2;
+        printf("Is your number %d?",num);
+        fflush(stdin);
+        scanf("%c",&ch);
+        if(ch=='h'){
+            ceil=num;
+        }else if(ch=='l'){
+            flr=num;
+        }else if(ch=='y'){
+            fl=0;
+        }
+    }
     printf("Your number is: %d.\nAnswered in: %d tries.",num,tries);
     return 0;
 }
