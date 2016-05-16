6e3c8e33e40427ab8463625305501f0ec69abfe4
kormin <abaotom14@gmail.com>
2016-05-16 20:23:31 +0800

    new file

---
+++ 3/caesarCipher.c
@@ -0,0 +1,21 @@
+/*
+ * Author: https://github.com/kormin
+ * Date Created: May 10, 2016
+ */
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main() {
+    int n, i, i2, len;
+    char x[50], x1[50];
+    char en[90], de[90];
+    printf("Enter shift: "); // requests for shift value
+    scanf("%d",&n);
+    printf("Enter your message: "); // requests message from user
+    fflush(stdin);
+    gets(x);
+    len = strlen(x);
+    return 0;
+}
