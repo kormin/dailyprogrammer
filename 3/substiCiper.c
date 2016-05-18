09a1c9180e2f329df56a33dacb8a5a747836267a
kormin <abaotom14@gmail.com>
2016-05-18 20:27:22 +0800

    add substicipher

---
+++ 3/substiCiper.c
@@ -0,0 +1,18 @@
+/*
+ * Author: https://github.com/kormin
+ * Date Created: May 10, 2016
+ */
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() {
+    char alph[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+    char alph1[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+    char key[50];
+    char msg[80], en[80], de[80];
+    int i, i1, i2, i3, len1, len2;
+    printf("Enter your keyword: "); // request for key
+    gets(key);
+    return 0;
+}
