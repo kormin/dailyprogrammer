f786127bdfd87d954bb2abf78f67a455b46898ac
kormin <abaotom14@gmail.com>
2016-05-18 20:28:06 +0800

    add modulo fx

--- 3/substiCiper.c
+++ 3/substiCiper.c
@@ -13,6 +13,20 @@
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
+
+/*
+* Modulo function to handle negative numbers
+* http://stackoverflow.com/questions/4003232/how-to-code-a-modulo-operator-in-c-c-obj-c-that-handles-negative-numbers
+*/
+int mod (int a, int b) {
+   if(b < 0) //you can check for b == 0 separately and do what you want
+     return mod(-a, -b);
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
+
 int main() {
     char alph[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
     char alph1[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
@@ -21,5 +35,9 @@ int main() {
     int i, i1, i2, i3, len1, len2;
     printf("Enter your keyword: "); // request for key
     gets(key);
+    len1 = strlen(alph); // alph len should be same as cipher len
+    len2 = strlen(key);
+    char cipher[len1];
+    int rept[len2];
     return 0;
 }
