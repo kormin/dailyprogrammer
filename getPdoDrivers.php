5e13b3c9266bc63674e5cc81c437af737c400e36
kormin <abaotom14@gmail.com>
2016-05-23 20:37:24 +0800

    add comments

--- getPdoDrivers.php
+++ getPdoDrivers.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Author: https://github.com/kormin
+ * Date Created: May 2016
+ * Description: 
+ * Resources:
+ *
+ */
 foreach (PDO::getAvailableDrivers() as $driver) {
 	echo "$driver <br>";
 }
